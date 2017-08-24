961b0842b10d613559c55e621b43983753b94d10
Rod Pattison <roderick.pattison@bhpbilliton.com>
2017-08-24 12:07:48 +0800

    Added vars to template

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,4 +7,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    tplVars = {'message': 'Welcome'}
+    return render(request, 'main/home.html', tplVars)
