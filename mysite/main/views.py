a8a9aafd3e7c38e24ea4f4f053b5d717237a90f9
Rod Pattison <roderick.pattison@bhpbilliton.com>
2017-08-24 12:05:30 +0800

    Added template

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-	return HttpResponse("Hello, World!")
+    return render(request, 'main/home.html')
