16a2f5a3a7cd20927aea82edbef7687507fb7218
Rod Pattison <rodpattison@gmail.com>
2017-09-02 08:41:43 +0800

    Created views as class-based

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^page1$', views.page1, name='page1'),
-    url(r'^page2$', views.page2, name='page2'),
+    # url(r'^$', views.home, name='home'),
+    # url(r'^page1$', views.page1, name='page1'),
+    # url(r'^page2$', views.page2, name='page2'),
+    url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^page1$', views.PageOne.as_view(), name='page1'),
+    url(r'^page2$', views.PageTwo.as_view(), name='page2')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,16 +2,32 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
+# from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
-def home(request):
-    tplVars = {'message': 'Welcome'}
-    return render(request, 'main/home.html', tplVars)
+# def home(request):
+#     tplVars = {'message': 'Welcome'}
+#     return render(request, 'main/home.html', tplVars)
+#
+# def page1(request):
+#     return render(request, 'main/page1.html')
+#
+# def page2(request):
+#     return render(request, 'main/page2.html')
 
-def page1(request):
-    return render(request, 'main/page1.html')
+class Home(View):
+    def get(self, request):
+        tplVars = {'message': 'Welcome'}
+        return render(request, 'main/home.html', tplVars)
 
-def page2(request):
-    return render(request, 'main/page2.html')
+
+class PageOne(View):
+    def get(self, request):
+        return render(request, 'main/page1.html')
+
+
+class PageTwo(View):
+    def get(self, request):
+        return render(request, 'main/page2.html')
