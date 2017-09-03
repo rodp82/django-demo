eb6bad217ec711b4b0a51d3c84de6486a7cd76aa
Rod Pattison <rodpattison@gmail.com>
2017-09-03 10:02:07 +0800

    Created models, generated the migration script and added models to admin

--- mysite/movieapp/admin.py
+++ mysite/movieapp/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Movie, Cast, Actor
+
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Actor)
+admin.site.register(Cast)
--- mysite/movieapp/models.py
+++ mysite/movieapp/models.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    year = models.IntegerField()
+    actors = models.ManyToManyField(Actor, through='Cast')
+
+    def __str__(self):
+        return '{} ({})'.format(self.title, self.year)
+
+
+class Cast(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return '{} - {}'.format(self.movie, self.actor)
