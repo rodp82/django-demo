86864db52ca65d851b6351f178c25c0e271b26ef
Rod Pattison <rodpattison@gmail.com>
2017-09-03 10:52:36 +0800

    Added a very basic movie list view

--- mysite/movieapp/views.py
+++ mysite/movieapp/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Movie
+
 
 # Create your views here.
+class MovieList(ListView):
+    model = Movie
+    context_object_name = 'movie_list'  # changes the name of default object_list name used in templates
