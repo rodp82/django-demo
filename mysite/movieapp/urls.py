2cd5897bcdfc405eecd2a2105629592a76ca43ff
Rod Pattison <rodpattison@gmail.com>
2017-09-03 15:07:10 +0800

    Added basic functionality to add/edit movies

--- mysite/movieapp/urls.py
+++ mysite/movieapp/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='movie_list', permanent=False), name='index'),
     url(r'^movies$', views.MovieList.as_view(), name='movie_list'),
+    url(r'^movies/create$', views.MovieCreate.as_view(), name='movie_create'),
     url(r'^movies/(?P<pk>\d+)$', views.MovieDetail.as_view(), name='movie_detail'),
+    url(r'^movies/(?P<pk>\d+)/update', views.MovieUpdate.as_view(), name='movie_update'),
     url(r'^actors$', views.ActorList.as_view(), name='actor_list'),
     url(r'^actors/(?P<pk>\d+)$', views.ActorDetail.as_view(), name='actor_detail'),
 ]
--- mysite/movieapp/views.py
+++ mysite/movieapp/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, UpdateView
+
 from .models import Movie, Actor
 
 
@@ -14,6 +16,28 @@ class MovieList(ListView):
 class MovieDetail(DetailView):
     model = Movie
     context_object_name = 'movie'
+
+
+class MovieCreate(CreateView):
+    model = Movie
+    fields = ['title', 'year']
+
+    def get_context_data(self, **kwargs):
+        """ Overrides the parent to get the context data and adds page_title to it """
+        context = super(MovieCreate, self).get_context_data(**kwargs)
+        context['page_title'] = 'Add Movie'
+        return context
+
+
+class MovieUpdate(UpdateView):
+    model = Movie
+    fields = ['title', 'year']
+
+    def get_context_data(self, **kwargs):
+        """ Overrides the parent to get the context data and adds page_title to it """
+        context = super(MovieUpdate, self).get_context_data(**kwargs)
+        context['page_title'] = 'Update Movie'
+        return context
 
 
 class ActorList(ListView):
