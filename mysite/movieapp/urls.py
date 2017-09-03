72b63dae2e18571d965e2d44cc5c46a00ac6cd09
Rod Pattison <rodpattison@gmail.com>
2017-09-03 15:13:49 +0800

    Added basic functionality to add/edit actors

--- mysite/movieapp/urls.py
+++ mysite/movieapp/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url(r'^movies/create$', views.MovieCreate.as_view(), name='movie_create'),
     url(r'^movies/(?P<pk>\d+)$', views.MovieDetail.as_view(), name='movie_detail'),
     url(r'^movies/(?P<pk>\d+)/update', views.MovieUpdate.as_view(), name='movie_update'),
+
     url(r'^actors$', views.ActorList.as_view(), name='actor_list'),
+    url(r'^actors/create$', views.ActorCreate.as_view(), name='actor_create'),
     url(r'^actors/(?P<pk>\d+)$', views.ActorDetail.as_view(), name='actor_detail'),
+    url(r'^actors/(?P<pk>\d+)/update', views.ActorUpdate.as_view(), name='actor_update'),
 ]
--- mysite/movieapp/views.py
+++ mysite/movieapp/views.py
@@ -48,3 +48,25 @@ class ActorList(ListView):
 class ActorDetail(DetailView):
     model = Actor
     context_object_name = 'actor'
+
+
+class ActorCreate(CreateView):
+    model = Actor
+    fields = ['name']
+
+    def get_context_data(self, **kwargs):
+        """ Overrides the parent to get the context data and adds page_title to it """
+        context = super(ActorCreate, self).get_context_data(**kwargs)
+        context['page_title'] = 'Add Actor'
+        return context
+
+
+class ActorUpdate(UpdateView):
+    model = Actor
+    fields = ['name']
+
+    def get_context_data(self, **kwargs):
+        """ Overrides the parent to get the context data and adds page_title to it """
+        context = super(ActorUpdate, self).get_context_data(**kwargs)
+        context['page_title'] = 'Update Actor'
+        return context
